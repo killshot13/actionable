531d9de2b9e51db6384cbd5eddf9fa74335c28c7
killshot13 <dmreh@outlook.com>
2020-12-22 03:26:27 -0600

    updated keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
-  mongoURI: `mongodb+srv://process.env.MONGOUSER:process.env.MONGOPASS@maincluster.po4fq.mongodb.net/process.env.MONGODB?retryWrites=true&w=majority`
+  mongoURI: `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@maincluster.po4fq.mongodb.net/${process.env.MONGODB}?retryWrites=true&w=majority`
 };
